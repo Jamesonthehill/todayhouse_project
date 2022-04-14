f83a031ca898990db568e80fc02538b3affff9ce
james <dlrjsdn53@naver.com>
2022-04-14 15:44:28 +0900

    2022.04.14 함수

---
+++ src/main/java/com/todayhouse/project/common/constants/Constants.java
@@ -0,0 +1,23 @@
+package com.todayhouse.project.common.constants;
+
+public final class Constants {
+
+	
+	//for paging
+	public static final int ROW_NUM_TO_SHOW = 10;
+	public static final int PAGE_NUM_TO_SHOW = 15;
+	
+	//insert 또는 update 후 페이지 전환 타입: 1: form , 2: list
+	public static final int INSERT_AFTER_TYPE = 1;
+	public static final int UPDATE_AFTER_TYPE=  1;
+	
+	public static final int SESSION_MINUTE = 30;
+
+	
+	public static final String URL_LOGINFORM = "/member/loginForm";
+	
+	public static final String DATETIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
+	public static final String DATE_FORMAT_BASIC = "yyyy-MM-dd";
+	
+	public static final int DATE_INTERVAL = -7;
+}
